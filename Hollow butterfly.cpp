2d6ffac4992dfe5f26479fe6cb06cf762448721b
struglingcoder <105796194+struglingcoder@users.noreply.github.com>
2022-05-21 11:27:47 +0530

    Create Hollow butterfly.cpp

---
+++ Hollow butterfly.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+        for (int i = 1; i <=5; i++) 
+        {
+            for(int k=1;k<=i;k++){
+                if(k==1 || k==i){
+                    cout<<'*';
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+            for(int j=2*(5-i+1);j>2;j--){
+                cout<<" ";
+            }
+            for(int k=1;k<=i;k++){
+                if(k==1 || k==i){
+                    cout<<'*';
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+            cout<<endl;
+        }  
+        for (int i = 5; i >=1; i--) 
+        {
+            for(int k=1;k<=i;k++){
+                if(k==1 || k==i){
+                    cout<<'*';
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+            for(int j=2*(5-i+1);j>2;j--){
+                cout<<" ";
+            }
+            for(int k=1;k<=i;k++){
+                if(k==1 || k==i){
+                    cout<<'*';
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+            cout<<endl;
+        }      
+        return 0;
+}

74e51c757425b6be94eeeff2048b5192b90a3279
struglingcoder <105796194+struglingcoder@users.noreply.github.com>
2022-05-21 10:50:08 +0530

    Create Pallindromic pattern.cpp

---
+++ Pallindromic pattern.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+        for (int i = 1; i <=5; i++) 
+        {
+            for(int j=5-i;j>=0;j--){
+                cout<<" ";
+            }
+            for(int k=i;k>=1;k--){
+                cout<<k;
+            }
+            for(int l=2;l<=i;l++){
+                cout<<l;
+            }
+            cout<<endl;
+        }
+        return 0;
+}

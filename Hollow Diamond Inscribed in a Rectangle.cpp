d66ee7c8f4a87ce3a95a0ea7dfb2d6e8589beea6
struglingcoder <105796194+struglingcoder@users.noreply.github.com>
2022-05-21 11:06:06 +0530

    Create Hollow Diamond Inscribed in a Rectangle.cpp

---
+++ Hollow Diamond Inscribed in a Rectangle.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+        for (int i = 1; i <=5; i++) 
+        {
+            for(int j=5-i;j>=1;j--){
+                cout<<"*";
+            }
+            for(int k=i;k>=1;k--){
+                if(k==i){
+                    cout<<'*';
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+            for(int l=2;l<=i;l++){
+                if(l==i){
+                    cout<<'*';
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+            for(int m=5-i;m>=1;m--){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+        for (int i = 5; i >=1; i--) 
+        {
+            for(int j=5-i;j>=1;j--){
+                cout<<"*";
+            }
+            for(int k=i;k>=1;k--){
+                if(k==i){
+                    cout<<'*';
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+            for(int l=2;l<=i;l++){
+                if(l==i){
+                    cout<<'*';
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+            for(int m=5-i;m>=1;m--){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+        return 0;
+}

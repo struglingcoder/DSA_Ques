c437f4aaee76d168bf76b85ce1d567533d4e80e7
struglingcoder <105796194+struglingcoder@users.noreply.github.com>
2022-06-24 17:33:10 +0530

    Update Chocolate Distr.. pblm.cpp

--- Chocolate Distr.. pblm.cpp
+++ Chocolate Distr.. pblm.cpp
@@ -21,3 +21,46 @@ int main()
     cout<<Min;
         return 0;
 }
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+    //code
+    long long ans=INT_MAX;
+    sort(a.begin(),a.end());
+    ans=min(ans,abs(a[m-1]-a[0]));
+    for(int i=m;i<n;i++){
+        ans=min(ans,abs(a[i]-a[i-m+1]));
+    }
+    return ans;
+    }   
+};
+
+// { Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends

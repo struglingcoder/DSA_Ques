ab06673493dfa269ff3f6a7154c12ce521ed52f2
struglingcoder <105796194+struglingcoder@users.noreply.github.com>
2022-07-17 17:00:45 +0530

    Update Max and Min Element in an Array.cpp

--- Max and Min Element in an Array.cpp
+++ Max and Min Element in an Array.cpp
@@ -1,28 +1,3 @@
-#include<iostream>
-#include<vector>
-#include<map>
-using namespace std;
-
-int main()
-{
-    int n;
-    cout<<"Enter the value of n ";
-    cin>>n;
-    int ar[n];
-    for(int i=0;i<n;i++){
-        cout<<"enter "<<i+1<<" element";
-        cin>>ar[i];
-    }
-    int Min=INT32_MAX;
-    int Max=INT32_MIN;
-    for(int i=0;i<n;i++){
-        Min=min(ar[i],Min);
-        Max=max(ar[i],Max);
-    }
-    cout<<"The minimum value element is --> "<<Min<<endl;
-    cout<<"The maximum value element is --> "<<Max<<endl;
-        return 0;
-}
 // { Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
@@ -44,7 +19,7 @@ int main() {
         cout << pp.first << " " << pp.second << endl;
     }
     return 0;
-}
+}// } Driver Code Ends
 
 pair<long long, long long> getMinMax(long long a[], int n) {
     pair<long long, long long> p;
